af6cd1acaf661da7a2b7ef8b9e0ce283a46ad40c
Adelina <adelina.vitamine@gmail.com>
2016-04-07 13:44:12 +0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OloloApp::Application.config.secret_key_base = 'bcc8fa817ac34f12ba4d180a3f683174693656a25b16af347a92658c24a4886b060dc1073447c460f60eb9f57f8abf2edc137599758dcce0cbc6b61ea9391e8d'
